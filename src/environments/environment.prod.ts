29b5dd01e1af0fa1756a6edc05663a14783115e1
rastafasha <mercadocreativo@gmail.com>
2023-09-19 14:12:04 -0400

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   // apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
   //apirest local node
   apiUrl: "http://localhost:5001/api",
-  apiUrlMedia: "http://localhost:5001/api/uploads",
+  apiUrlMedia: "http://localhost:5001/api/uploads/",
   //apirest local
   // apiUrl: "http://localhost:8888/proyectos/backend-api-daniel/public/api",
   // apiUrlMedia: "http://localhost:8888/proyectos/backend-api-daniel/storage/app/",
@@ -15,5 +15,5 @@ export const environment = {
   clientSandboxId: 'AXlazeNsZ0CmjfJIronSzcqzw4hLHkcoVEM5fO5BY7AbD-_GhKoKezRcavq6-T4kQuRqaTXFB_VXmheG',
   clientId: '',
   clientGoogle: '291137676127-svvuuca518djs47q2v78se9q6iggi4nq.apps.googleusercontent.com',
-  paypalKey:'AQhKPBY5mgg0JustLJCcf6ncmd9RghCiNhXT_b6rNUakyQtnEn8MzCn_dkHAyt5n7_P0Omo5M05to5j0'
+  paypalKey:'Aebb_SJ2-L8OrgRAvAERINfyMb7eKrqZ7xPt5JreBd9eYfDjzfDildStuo5Gjcx6GNvWbTZaiwBiMeAf'
 };
