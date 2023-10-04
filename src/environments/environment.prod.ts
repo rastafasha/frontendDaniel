dff88a12bfbff40a578df88b453b9230732fde9c
rastafasha <mercadocreativo@gmail.com>
2023-10-04 14:04:18 -0400

    conectado a vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   //apirest local node
-  apiUrl: "http://localhost:5001/api",
-  apiUrlMedia: "http://localhost:5001/api/uploads/",
+   // apiUrl: "http://localhost:5001/api",
+  // apiUrlMedia: "http://localhost:5001/api/uploads/",
   //remoto
-  // apiUrl: "https://backend-daniel-node-mongo.vercel.app/api",
-  // apiUrlMedia: "https://backend-daniel-node-mongo.vercel.app/storage/app/",
+  apiUrl: "https://backend-daniel-node-mongo.vercel.app/api",
+  apiUrlMedia: "https://backend-daniel-node-mongo.vercel.app/api/uploads/",
   clientSandboxId: 'AXlazeNsZ0CmjfJIronSzcqzw4hLHkcoVEM5fO5BY7AbD-_GhKoKezRcavq6-T4kQuRqaTXFB_VXmheG',
   clientId: '',
   clientGoogle: '291137676127-svvuuca518djs47q2v78se9q6iggi4nq.apps.googleusercontent.com',
