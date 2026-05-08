7c274bad1d0cced0aa1e4cf7e1f74caaaa7e10e2
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-05-08 10:50:48 -0400

    actualizado emv

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   mediaUrlRemoto: 'https://res.cloudinary.com/dmv6aukai/image/upload/v1741218430/articlesApp',
   //paypal
   clientSandboxId: 'AXlazeNsZ0CmjfJIronSzcqzw4hLHkcoVEM5fO5BY7AbD-_GhKoKezRcavq6-T4kQuRqaTXFB_VXmheG',
-  paypalKey:'EPrUzIEuBe0CjvIr7AbBfWGEjW2eotI8cq2UHtJ_YsZp47H5K0-QXib6joqRhZ8H14U7sTKg7iGeRi-Y',
+  paypalKey:'Aebb_SJ2-L8OrgRAvAERINfyMb7eKrqZ7xPt5JreBd9eYfDjzfDildStuo5Gjcx6GNvWbTZaiwBiMeAf',
   clientId: '',
   //google
   clientGoogle: '291137676127-svvuuca518djs47q2v78se9q6iggi4nq.apps.googleusercontent.com',
